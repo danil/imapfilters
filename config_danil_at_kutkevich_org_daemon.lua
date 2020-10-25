88859ab6844f6eab4cddc741deaf8466a71e674c
Danil Kutkevich <danil@kutkevich.org>
2020-10-25 16:45:10 +0000

    fixing filter demonizatoin

--- config_danil_at_kutkevich_org_daemon.lua
+++ config_danil_at_kutkevich_org_daemon.lua
@@ -7,15 +7,10 @@ require "config/danil_at_kutkevich_org"
 -- IMAPFilter can be detached from the controlling terminal and run in
 -- the background as a system daemon.
 --
--- The auxiliary function daemon_mode() is supplied for conveniency.
--- The following example puts imapfilter in the background and runs
--- endlessly, executing the commands in the forever() function and
--- sleeping for 600 seconds between intervals:
-function forever()
+-- Waits for a notification by the server when new messages arrive in
+-- the monitored using the IMAP IDLE extension.
+while true do
   local mailbox = danil_at_kutkevich_org._new
-  repeat
-  -- pcall(filtering_danil_at_kutkevich_org)
+  mailbox:enter_idle()
   filtering_danil_at_kutkevich_org(danil_at_kutkevich_org)
-  until not mailbox:enter_idle()
 end
-become_daemon(1, forever)
